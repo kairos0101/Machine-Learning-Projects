13991b26ae6b567974d2ea7b4705e0e6a20455a2
Jayde Mirandilla <110360105+kairos0101@users.noreply.github.com>
2024-05-11 23:28:09 +0800

    Add files via upload

---
+++ Diabetes-Classification/utils_diabetes.py
@@ -0,0 +1,76 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import ShuffleSplit
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+import pandas as pd
+
+
+def find_best_model(X, y):
+    models = {
+        'logistic_regression': {
+            'model': LogisticRegression(solver='lbfgs', multi_class='auto'),
+            'parameters': {
+                'C': [1,5,10]
+               }
+        },
+
+        'decision_tree': {
+            'model': DecisionTreeClassifier(splitter='best'),
+            'parameters': {
+                'criterion': ['gini', 'entropy'],
+                'max_depth': [5,10]
+            }
+        },
+
+        'random_forest': {
+            'model': RandomForestClassifier(criterion='gini'),
+            'parameters': {
+                'n_estimators': [10,15,20,50,100,200]
+            }
+        },
+
+        'svm': {
+            'model': SVC(gamma='auto'),
+            'parameters': {
+                'C': [1,10,20],
+                'kernel': ['rbf','linear']
+            }
+        }
+
+    }
+
+    scores = []
+    cv_shuffle = ShuffleSplit(n_splits=5, test_size=0.20, random_state=0)
+
+    for model_name, model_params in models.items():
+        gs = GridSearchCV(model_params['model'], model_params['parameters'], cv = cv_shuffle, return_train_score=False)
+        gs.fit(X, y)
+        scores.append({
+            'model': model_name,
+            'best_parameters': gs.best_params_,
+            'score': gs.best_score_
+        })
+
+    return pd.DataFrame(scores, columns=['model','best_parameters','score'])
+
+'''
+def predict_diabetes(classifier, Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DPF, Age):
+    preg = int(Pregnancies)
+    glucose = float(Glucose)
+    bp = float(BloodPressure)
+    st = float(SkinThickness)
+    insulin = float(Insulin)
+    bmi = float(BMI)
+    dpf = float(DPF)
+    age = int(Age)
+
+    x = [[preg, glucose, bp, st, insulin, bmi, dpf, age]]
+    sc = StandardScaler()
+    x = sc.transform(x)
+
+    return classifier.predict(x)
+
+'''
